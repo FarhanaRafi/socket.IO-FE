6b0304bb4d212f95afda2c3ac62943d0c2941ce5
FarhanaRafi <118438210+FarhanaRafi@users.noreply.github.com>
2023-04-13 15:28:14 +0200

    done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,53 @@ import {
   Form,
   FormControl,
   ListGroup,
-  Button,
 } from "react-bootstrap"
+import { io } from 'socket.io-client'
+import { User, Message } from "../types"
+
+const socket = io("http://localhost:3001", {transports: ["websocket"]})
 
 const Home = () => {
   const [username, setUsername] = useState("")
   const [message, setMessage] = useState("")
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [chatHistory, setChatHistory] = useState<Message[]>([])
+
+  useEffect(() => {
+    socket.on("welcome", welcomeMessage => {
+      console.log(welcomeMessage)
+
+      socket.on("loggedIn", onlineUsersList => {
+        console.log(onlineUsersList)
+        setOnlineUsers(onlineUsersList)
+        setLoggedIn(true)
+      })
+
+      socket.on("updateOnlineUsersList", updatedList => {
+        setOnlineUsers(updatedList)
+      })
+
+      socket.on("newMessage", newMessage => {
+        console.log(newMessage)
+        setChatHistory((chatHistory) => [...chatHistory, newMessage.message])
+      })
+    })
+  }, [])
+
+  const submitUsername = () => {
+    socket.emit("setUsername", { username })
+  }
+
+  const sendMessage = () => {
+    const newMessage = {
+      sender: username,
+      text: message,
+      createdAt: new Date().toLocaleString("en-US")
+    }
+    socket.emit("sendMessage", { message: newMessage })
+    setChatHistory([...chatHistory, newMessage])
+  }
 
   return (
     <Container fluid>
@@ -23,33 +64,46 @@ const Home = () => {
           <Form
             onSubmit={e => {
               e.preventDefault()
+              submitUsername()
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
               onChange={e => setUsername(e.target.value)}
+              disabled={loggedIn}
             />
           </Form>
           {/* )} */}
           {/* MIDDLE AREA: CHAT HISTORY */}
-          <ListGroup></ListGroup>
+          <ListGroup>
+          {chatHistory.map((message, index) => (<ListGroup.Item key={index}>{<strong className="text-success">{message.sender} </strong>} <br /> {message.text} <br /> <small style={{display:'flex', justifyContent:'right'}}>{message.createdAt}</small></ListGroup.Item>))}
+          </ListGroup>
           {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
             onSubmit={e => {
               e.preventDefault()
+              sendMessage()
             }}
           >
             <FormControl
               placeholder="Write your message here"
               value={message}
               onChange={e => setMessage(e.target.value)}
+              disabled={!loggedIn}
             />
           </Form>
         </Col>
         <Col md={3}>
           {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
+          {onlineUsers.length === 0 && (
+            <ListGroup.Item>Log in to check who's online!</ListGroup.Item>
+          )}
+          <ListGroup>
+
+{onlineUsers.map(user => (<ListGroup.Item key={user.socketId}>{user.username}</ListGroup.Item>))}
+</ListGroup>
         </Col>
       </Row>
     </Container>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export type User = {
   username: string
-  id: string
-  room: "blue" | "red"
+  socketId: string
+  // room: "blue" | "red"
 }
 
 export type Message = {
